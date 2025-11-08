ae27f5370ca33861bf41c3db06c38fa32725607a
Jh0nz1 <22403132@aluno.cotemig.com.br>
2025-11-08 19:09:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const themeToggle = document.getElementById('theme-toggle');
+const htmlElement = document.body;
+
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  htmlElement.setAttribute('data-theme', savedTheme);
+}
+
+themeToggle.addEventListener('click', () => {
+  const currentTheme = htmlElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+  htmlElement.setAttribute('data-theme', newTheme);
+  localStorage.setItem('theme', newTheme); 
+});
+
+function salvar() {
+      
+      let nome = document.getElementById("nome").value;
+      let email = document.getElementById("email").value;
+
+      
+      let usuario = {
+        nome: nome,
+        email: email
+      };
+
+     
+      localStorage.setItem("usuario", JSON.stringify(usuario));
+
+      alert("Informação salva com sucesso!");
+    }
+
+    function mostrar() {
+      
+      let usuario = localStorage.getItem("usuario");
+
+      
+      usuario = JSON.parse(usuario);
+
+      
+      document.getElementById("resultado").innerText =
+        ` Nome: ${usuario.nome} | Email: ${usuario.email}`;
+    }
+
